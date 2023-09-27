5bbe0a1a1bd5c4c0345f2f1e1d56ef92f9e02f69
sangeeta101sharma <sangita.sharma771998@gmail.com>
2023-09-27 10:45:43 +0530

    create department page

--- department.php
+++ department.php
@@ -150,7 +150,7 @@
                         1 : !s("\ud83c\uddfa\ud83c\uddf3", "\ud83c\uddfa\u200b\ud83c\uddf3") && !s(
                             "\ud83c\udff4\udb40\udc67\udb40\udc62\udb40\udc65\udb40\udc6e\udb40\udc67\udb40\udc7f",
                             "\ud83c\udff4\u200b\udb40\udc67\u200b\udb40\udc62\u200b\udb40\udc65\u200b\udb40\udc6e\u200b\udb40\udc67\u200b\udb40\udc7f"
-                            );
+                        );
                 case "emoji":
                     return !s("\ud83e\udef1\ud83c\udffb\u200d\ud83e\udef2\ud83c\udfff",
                         "\ud83e\udef1\ud83c\udffb\u200b\ud83e\udef2\ud83c\udfff")
@@ -579,7 +579,7 @@
 include_once('include/header.php');
 ?>
     <section>
-    <div class="inner-banner">
+        <div class="inner-banner">
             <img src="wp-content/uploads/2019/11/about_banner.jpg" alt="About Us" title="Organ Hospital">
         </div>
         <div class="breadcrumb_outer">
@@ -592,6 +592,335 @@ include_once('include/header.php');
         </div>
         <div class="clearfix"></div>
     </section>
+    <section>
+        <div class="div-mtb">
+            <div class="container">
+                <div class="medical-services"
+                    style="background-image: url('wp-content/uploads/2019/11/Medical-Services-a.jpg');">
+                    <div class="medical-services-1">
+                        <div class="medical-services-2">
+                            <h2 class="title-3">Medical Department</h2>
+                            <div class="nav-link-1">
+                                <ul>
+                                    <li>Emergency Department (ED)</li>
+                                    <li>Pharmacy</li>
+                                    <li>Laboratory</li>
+                                    <li>Radiology</li>
+                                    <li>Oncology</li>
+                                    <li>Neurology</li>
+                                    <li>Orthopedics</li>
+                                    <li>Obstetrics and Gynecology (OB/GYN)</li>
+                                    <li>Pediatrics</li>
+                                    <li>Intensive Care Unit (ICU)</li>
+                                    <li>Medical/Surgical Unit</li>
+                                    <li>Maintenance and Facilities</li>
+                                    <li>Administration/Management</li>
+                                    <li>Infection Control </li>
+                                    <li>Social Services</li>
+                                    <li>Respiratory Therapy</li>
+                                    <li>Nutrition and Dietary Services</li>
+                                    <li>Psychiatry/Psychology</li>
+                                    <li>Physical Therapy/Rehabilitation</li>
+                                </ul>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+               
+
+
+            </div>
+        </div>
+    </section>
+    <!-- Card Start -->
+    <style>
+        @import "https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,500,700,700i|Raleway:300,400,500,700,800";
+
+.body {
+  background: #f9fbff;
+}
+a:hover {
+  text-decoration: none;
+}
+.blog-card {
+  display: flex;
+  flex-direction: column;
+  margin: 1rem auto;
+  box-shadow: 4px 15px 25px -12px rgba(152, 182, 255, 0.4);
+  margin-bottom: 1.6%;
+  background: #fff;
+  line-height: 1.4;
+  border-radius: 6px;
+  overflow: hidden;
+  z-index: 0;
+}
+.blog-card a {
+  color: inherit;
+}
+.blog-card a:hover {
+  color: #ffc400;
+}
+.blog-card:hover .photo {
+  -webkit-transform: scale(1.15) rotate(3deg);
+  transform: scale(1.15) rotate(3deg);
+}
+.blog-card .meta {
+  position: relative;
+  z-index: 0;
+  height: 200px;
+}
+.blog-card .photo {
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-size: cover;
+  background-position: center;
+  transition: -webkit-transform 0.2s;
+  transition: transform 0.2s;
+  transition: transform 0.2s, -webkit-transform 0.2s;
+}
+.blog-card .description {
+  padding: 1rem;
+  background: #fff;
+  position: relative;
+  /* z-index: 1; */
+}
+.blog-card .description h1 {
+  line-height: 1.35;
+  margin: 0;
+  font-weight: 500;
+  font-size: 1.1rem;
+  font-family: "open sans";
+}
+.blog-card .description h2 {
+  font-size: 0.9rem;
+  font-weight: 300;
+  color: #a2a2a2;
+  margin-top: 5px;
+  font-family: "open sans";
+}
+.blog-card .description .read-more {
+  padding-top: 8px;
+  left: 0;
+  right: 0;
+  text-align: center;
+  bottom: 6px;
+  position: absolute;
+  border-top: 1px solid #dadada;
+  color: #3ebecc;
+}
+.blog-card .description .read-more a {
+  color: #3ebecc;
+  font-size: 16px;
+  display: inline-block;
+  position: relative;
+  /*  border: 1px solid;
+  padding: 2px 10px; */
+  border-radius: 4px;
+  text-transform: uppercase;
+}
+.blog-card .description .read-more:after {
+  content: "\f061";
+  font-family: FontAwesome;
+  margin-left: -9px;
+  opacity: 0;
+  vertical-align: middle;
+  transition: margin 0.3s, opacity 0.3s;
+}
+.blog-card .description .read-more:hover:after {
+  margin-left: 10px;
+  opacity: 1;
+}
+.blog-card p {
+  position: relative;
+  margin: 10px 0 0;
+}
+.blog-card p:first-of-type {
+  margin-top: 1.25rem;
+}
+
+.blog-card:hover .details {
+  left: 0%;
+}
+@media (min-width: 640px) {
+  .blog-card {
+    flex-direction: row;
+    min-height: 130px;
+  }
+  .blog-card .meta {
+    flex-basis: 30%;
+    height: auto;
+  }
+  .blog-card .description {
+    flex-basis: 70%;
+  }
+  .blog-card .description:before {
+    -webkit-transform: skewX(-3deg);
+    transform: skewX(-3deg);
+    content: "";
+    background: #fff;
+    width: 30px;
+    position: absolute;
+    left: -10px;
+    top: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+  .blog-card.alt {
+    flex-direction: row-reverse;
+  }
+  .blog-card.alt .description:before {
+    left: inherit;
+    right: -10px;
+    -webkit-transform: skew(3deg);
+    transform: skew(3deg);
+  }
+  .blog-card.alt .details {
+    padding-left: 25px;
+  }
+}
+
+    </style>
+    <div class="container-fluid px-lg-5 body">
+  <div class="row justify-content-center">
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(images/slider_img2.jpg);height:400px;width:200px;background-size: cover; background-repeat: no-repeat; background-position: left;"></div>
+        </div>
+        <div class="description">
+          <h1>Emergency Department (ED)</h1>
+          <h2 class="pt-1 pb-4">: Also known as the Emergency Room, this department provides immediate medical care to patients with acute and life-threatening conditions.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(images/slider_img4.jpg)"></div>
+        </div>
+        <div class="description">
+          <h1>Intensive Care Unit (ICU)</h1>
+          <h2 class="pt-1 pb-4">The ICU is where critically ill patients receive specialized care, often with advanced monitoring and life-support equipment.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(images/slider_size2.jpeg)"></div>
+        </div>
+        <div class="description">
+          <h1>Medical-Surgical Units</h1>
+          <h2 class="pt-1 pb-4"> These units provide care for patients with a wide range of medical and surgical conditions, including post-operative care.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300)"></div>
+        </div>
+        <div class="description">
+          <h1>Pediatrics</h1>
+          <h2 class="pt-1 pb-4"> Specialized care for infants, children, and adolescents is provided in the pediatric department.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1576659325478-807b9c6757d8?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300)"></div>
+        </div>
+        <div class="description">
+          <h1>Neurology</h1>
+          <h2 class="pt-1 pb-4"> Neurology departments focus on the diagnosis and treatment of neurological disorders such as epilepsy, stroke, and multiple sclerosis.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1490367532201-b9bc1dc483f6?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300)"></div>
+        </div>
+        <div class="description">
+          <h1>Radiology</h1>
+          <h2 class="pt-1 pb-4"> Radiology departments perform various imaging tests such as X-rays, CT scans, and MRIs.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1476231790875-016a80c274f3?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300)"></div>
+        </div>
+        <div class="description">
+          <h1>Pharmacy </h1>
+          <h2 class="pt-1 pb-4"> Hospitals have on-site pharmacies to dispense medications prescribed by physicians.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300)"></div>
+        </div>
+        <div class="description">
+          <h1>Respiratory Therapy</h1>
+          <h2 class="pt-1 pb-4"> Respiratory therapists assist patients with breathing difficulties, such as asthma or those on ventilators.
+
+</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-6 d-flex">
+      <div class="blog-card w-100">
+        <div class="meta">
+          <div class="photo" style="background-image: url(https://images.unsplash.com/photo-1460925895917-afdab827c52f?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=500&ixid=eyJhcHBfaWQiOjF9&ixlib=rb-1.2.1&q=50&w=300)"></div>
+        </div>
+        <div class="description">
+          <h1>Infection Control</h1>
+          <h2 class="pt-1 pb-4"> This department focuses on preventing and controlling hospital-acquired infections.</h2>
+          <p class="read-more">
+            <a href="#" target="_blank">Click Here</a>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+    <!-- Card End -->
+    
     <!-- Footer -->
     <?php 
 include_once('include/footer.php');
@@ -1044,4 +1373,5 @@ include_once('include/footer.php');
     <script type='text/javascript' src='wp-content/themes/organhospital/js/script68c3.js?ver=20151811'
         id='custom-script-js'></script>
 </body>
+
 </html>
